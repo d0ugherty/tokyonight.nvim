3e964577f8926686dcc805d6b7475a92a804d870
Thomas Dougherty <tdougherty16@gmail.com>
2023-10-01 17:17:11 -0400

    Update colors.lua

--- lua/tokyonight/colors.lua
+++ lua/tokyonight/colors.lua
@@ -5,7 +5,7 @@ local M = {}
 ---@class Palette
 M.default = {
   none = "NONE",
-  bg_dark = "##1a1b26",
+  bg_dark = "#1a1b26",
   bg = "#1a1b26",
   bg_highlight = "#292e42",
   terminal_black = "#414868",
