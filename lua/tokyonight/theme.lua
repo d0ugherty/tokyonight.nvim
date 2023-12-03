28c757a0dbc6d494909a5d610493067bca54da8e
Thomas Dougherty <tdougherty16@gmail.com>
2023-12-02 20:17:44 -0500

    Update theme.lua

--- lua/tokyonight/theme.lua
+++ lua/tokyonight/theme.lua
@@ -213,7 +213,7 @@ function M.setup()
 
     --- Punctuation
     ["@punctuation.delimiter"] = { fg = c.blue5 }, -- For delimiters ie: `.`
-    ["@punctuation.bracket"] = { fg = c.red1 }, -- For brackets and parens.
+    ["@punctuation.bracket"] = { fg = c.red }, -- For brackets and parens.
     ["@punctuation.special"] = { fg = c.blue5 }, -- For special punctutation that does not fall in the catagories before.
     ["@punctuation.special.markdown"] = { fg = c.orange, bold = true },
 
